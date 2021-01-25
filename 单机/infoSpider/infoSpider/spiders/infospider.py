a22267d1e65caa781227fba4862f5dc85998e661
Kayden Cheung <993963526@qq.com>
2021-01-25 08:15:46 +0800

    Update infospider.py

--- 单机/infoSpider/infoSpider/spiders/infospider.py
+++ 单机/infoSpider/infoSpider/spiders/infospider.py
@@ -6,11 +6,7 @@ from infoSpider.items import InfospiderItem
 class InfospiderSpider(scrapy.Spider):
     name = 'infospider'
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
-        # 'Cookie': '_T_WM=29225193880; BAIDU_SSP_lcr=https://www.google.com/; '
-        #           'SUB=_2A25NDQEODeRhGeNH61UV8C3Izj6IHXVu8a9GrDV6PUJbktANLXWjkW1NSvegmyBX0oPqVdafKYiBnxRUArl4EwHK; '
-        #           'SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9W5.q_dJWDLANz.-gZ9xXzds5NHD95Qf1K5NSh50Sh-EWs4Dqcj_i--Xi-zRi'
-        #           '-zci--ci-zpiKLsi--fi-zNi-2ci--4iK.Ei-z7i--4iKn0iKy8; SSOLoginState=1611231582 '
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'
     }
 
     start_urls = []
@@ -27,12 +23,9 @@ class InfospiderSpider(scrapy.Spider):
 
     def parse(self, response):
         infos = response.xpath('//div[@class="card-wrap" and not(div[@class="card-top"])]')
-        # infos = div.xpath('//p[@class="txt" and @node-type="feed_list_content"]')
 
         for info in infos:
             item = InfospiderItem()
-            # item['content'] = info.xpath(
-            #     './/p[@class="txt" and @node-type="feed_list_content"]').xpath('string(.)').extract()[0].strip()
             text = info.xpath('.//p[@class="txt" and @node-type="feed_list_content"]')
             if text:
                 item['content'] = text.xpath(
