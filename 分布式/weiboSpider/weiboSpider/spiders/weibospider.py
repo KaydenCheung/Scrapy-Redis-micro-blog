06810d8e487046862757423fe15fb180867d13ff
Kayden Cheung <993963526@qq.com>
2021-01-25 08:22:03 +0800

    Update weibospider.py

--- 分布式/weiboSpider/weiboSpider/spiders/weibospider.py
+++ 分布式/weiboSpider/weiboSpider/spiders/weibospider.py
@@ -8,11 +8,7 @@ class WeibospiderSpider(scrapy.Spider):
     name = 'weibospider'
     start_urls = ['https://s.weibo.com/top/summary?cate=realtimehot']
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
-        # 'Cookie': '_T_WM=29225193880; BAIDU_SSP_lcr=https://www.google.com/; '
-        #           'SUB=_2A25NDQEODeRhGeNH61UV8C3Izj6IHXVu8a9GrDV6PUJbktANLXWjkW1NSvegmyBX0oPqVdafKYiBnxRUArl4EwHK; '
-        #           'SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9W5.q_dJWDLANz.-gZ9xXzds5NHD95Qf1K5NSh50Sh-EWs4Dqcj_i--Xi-zRi'
-        #           '-zci--ci-zpiKLsi--fi-zNi-2ci--4iK.Ei-z7i--4iKn0iKy8; SSOLoginState=1611231582 '
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'
     }
 
     def start_requests(self):
@@ -37,5 +33,3 @@ class WeibospiderSpider(scrapy.Spider):
             f.write(url)
             f.write('\n')
             yield item
-
-
