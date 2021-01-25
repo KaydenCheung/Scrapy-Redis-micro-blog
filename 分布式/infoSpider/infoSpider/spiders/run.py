b591346f80e9345b787023c4613ef2cea0ed0871
Kayden Cheung <993963526@qq.com>
2021-01-25 08:28:37 +0800

    Update run.py

--- 分布式/infoSpider/infoSpider/spiders/run.py
+++ 分布式/infoSpider/infoSpider/spiders/run.py
@@ -4,7 +4,7 @@ import redis
 
 if __name__ == '__main__':
     # os.system('pwd')
-    r = redis.Redis(host='47.102.204.46', port=6379, decode_responses=True)
+    r = redis.Redis(host='46.100.201.42', port=6379, decode_responses=True)
     while True:
         os.system("scrapy runspider infospider.py")
         time.sleep(2)
